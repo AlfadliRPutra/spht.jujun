61c00aa6400fbbeebb9a221e309960a79177f3c9
Iqbaalhaa <iqbalhaanafi@gmail.com>
2026-04-22 18:38:44 +0700

    remove ProductSeeder from DatabaseSeeder and delete ProductSeeder file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CategorySeeder::class,
-            ProductSeeder::class,
             HeroSeeder::class,
         ]);
     }
